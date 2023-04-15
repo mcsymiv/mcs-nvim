e8276e6d590d6815d6af60d2475720f2cd9ae68d
mcsymiv <s.mcsymiv@gmail.com>
2023-04-15 20:36:40 +0300

    add go autoimport cmd

--- lua/mcs/autocmd.lua
+++ lua/mcs/autocmd.lua
@@ -18,3 +18,10 @@ api.nvim_create_autocmd(
   { "InsertEnter", "WinLeave" },
   { pattern = "*", command = "set nocursorline", group = cursorGrp }
 )
+
+api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*.go',
+  callback = function()
+    vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
+  end
+})
--- lua/mcs/plugins/lsp.lua
+++ lua/mcs/plugins/lsp.lua
@@ -32,14 +32,13 @@ local M = {
       -- NOTE: Remember that lua is a real programming language, and as such it is possible
       -- to define small helper and utility functions so you don't have to repeat yourself
       -- many times.
-      --
       -- In this case, we create a function that lets us more easily define mappings specific
       -- for LSP related items. It sets the mode, buffer and description for us each time.
       local nmap = function(keys, func, desc)
         if desc then
           desc = 'LSP: ' .. desc
         end
-	      vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+	      vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc, noremap = true })
       end
 
       nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
@@ -48,7 +47,7 @@ local M = {
       nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
       nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
       nmap('gi', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-      nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+      nmap('<leader>gf', vim.lsp.buf.type_definition, 'Type [D]efinition')
       nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
       nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
