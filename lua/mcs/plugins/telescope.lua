618c19487992d3939bef51a0e34c98b07bea9291
mcsymiv <s.mcsymiv@gmail.com>
2023-05-06 10:22:14 +0300

    add telescope picker hidden files, find string key

--- lua/mcs/plugins/telescope.lua
+++ lua/mcs/plugins/telescope.lua
@@ -18,6 +18,7 @@ local M = {
     { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "[f]ind [b]uffers" },
     { "<leader>fs", "<cmd>Telescope grep_string<cr>", desc = "[f]ind [s]tring" },
     { "<leader>gs", "<cmd>Telescope git_status<cr>", desc = "[g]it [s]tatus" },
+    { "<leader>fS", "<cmd>Telescope grep_string<cr> . \"'\" . expand('<cword>')", desc = "[f]ind [S]tring under cursor" },
     -- file_browser extension remaps
     { "<leader>b", "<cmd>Telescope file_browser path=%:p:h select_buffer=true<cr>", desc = "[g]it [s]tatus", { noremap = true } },
   },
@@ -26,9 +27,15 @@ local M = {
     local telescope = require("telescope")
 
     telescope.setup({
+      pickers = {
+        find_files = {
+            hidden = true
+        },
+      },
       defaults = {
         file_ignore_patterns = {
-          "node_modules"
+          "node_modules",
+          ".git/",
         },
         layout_strategy = "vertical",
         layout_config = {
