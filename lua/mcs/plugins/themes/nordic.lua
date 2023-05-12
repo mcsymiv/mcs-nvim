a7e393c1f6609713d5247036f8a02e4bd221b464
mcsymiv <s.mcsymiv@gmail.com>
2023-05-12 15:12:32 +0300

    nordic format, refactor

--- lua/mcs/plugins/themes/nordic.lua
+++ lua/mcs/plugins/themes/nordic.lua
@@ -1,23 +1,34 @@
+local blue = {
+  light = "#82b5ba",
+  dark = "#41484d",
+}
+
 local nord = require('nordic')
 nord.setup({
   -- Available themes: 'nordic', 'onedark'.
   -- Onedark is WIP.
   theme = 'nordic',
+
   -- Enable bold keywords.
   bold_keywords = true,
+
   -- Enable italic comments.
   italic_comments = true,
+
   -- Enable general editor background transparency.
   transparent_bg = false,
+
   -- Enable brighter float border.
   bright_border = true,
+
   -- Nordic specific options.
   -- Set all to false to use original Nord colors.
   -- Adjusts some colors to make the theme a bit nicer (imo).
   nordic = {
       -- Reduce the overall amount of blue in the theme (diverges from base Nord).
-      -- reduced_blue = true,
+    reduced_blue = true,
   },
+
   -- Onedark specific options.
   -- Set all to false to keep original onedark colors.
   -- Adjusts some colors to make the theme a bit nicer (imo).
@@ -26,25 +37,21 @@ nord.setup({
       -- Brighten the whites to fit the theme better.
       -- brighter_whites = true,
   },
+
   -- Override the styling of any highlight group.
   override = {
+    -- Override of autocompletion styling
     NormalFloat = {
-      fg = "#82b5ba",
-      bg = "#41484d",
+      fg = blue.light,
+      bg = blue.dark,
     }, -- Normal text in floating windows. Text in second after Pmenu popup
----    NormalSB = {
----      fg = "#82b5ba",
----      bg = "#41484d",
----    }, -- normal text in sidebar
----    FloatBorder = {
----      fg = "#82b5ba",
----      bg = "#41484d",
----    },
+
     Pmenu = {
-      fg = "#82b5ba",
-      bg = "#41484d",
+      fg = blue.light,
+      bg = blue.dark,
     }, -- Popup menu
   },
+
   cursorline = {
       -- Enable bold font in cursorline.
       bold = false,
@@ -53,17 +60,22 @@ nord.setup({
       -- Hide the cursorline when the window is not focused.
       hide_unfocused = true,
   },
+
   noice = {
       -- Available styles: `classic`, `flat`.
       style = 'classic',
   },
+
   telescope = {
       -- Available styles: `classic`, `flat`.
-      style = 'classic',
+      style = 'flat',
   },
+
   leap = {
       -- Dims the backdrop when using leap.
       dim_backdrop = false,
   },
 })
+
+-- Nordic configuration load
 nord.load()
