3ad104f65d0b244bf24c87c760ad9e2256976636
mcsymiv <mcs@mcsymivs-MacBook-Pro.local>
2025-04-06 12:52:12 +0300

    upd lspconfig

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -1,15 +1,15 @@
 return {
   {
-    "nvim-treesitter/nvim-treesitter", 
+    "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function () 
+    config = function ()
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-	ensure_installed = { "lua", "vim", "vimdoc", "javascript", "typescript" },
+	ensure_installed = { "lua", "vim", "vimdoc", "javascript", "typescript", "go" },
 	sync_install = false,
 	highlight = { enable = true },
-	indent = { enable = true },  
+	indent = { enable = true },
       })
     end
   },
