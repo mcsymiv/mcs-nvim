55daff2a1ea09df96dee513595ad0f6366c7f847
mcsymiv <mcs@mcsymivs-MacBook-Pro.local>
2025-04-06 12:35:57 +0300

    add telescope

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -4,8 +4,38 @@ return {
 -- or                              , branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function ()
+      local telescope = require("telescope")
+
+      telescope.setup({
+	defaults = {
+	  layout_strategy = "vertical",
+
+	  layout_config = {
+	    width = 0.95,
+	    height = 0.85,
+	  }, -- layout config
+
+	}, -- defualts
+
+	pickers = {
+	  find_files = {
+	    mappings = {
+	      i = {
+		["<C-h>"] = "select_horizontal", -- opens found file in horizontal window(?), :sp file. works as <C-ss>
+	      },
+	    },
+	    find_command = { "rg", "--files", "--hidden", "--no-ignore-vcs", "--glob", "!**/.git/*" },
+	  },
+	},
+
+	extensions = {
+	  fzf = {}
+	},
+
+      }) -- telescope setup end
+
+      telescope.load_extension 'fzf'
 
     end
-
   }
 }
